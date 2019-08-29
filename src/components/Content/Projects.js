8688d2a8b3ca13e9c3bf8c59a185e4d94e137e39
Yves Beutler <yves.beutler@interdiscount.ch>
2019-08-29 15:41:36 +0200

    add basic project overlay

--- src/components/Content/Projects.js
+++ src/components/Content/Projects.js
@@ -3,14 +3,29 @@ import React from 'react';
 function Introduction() {
     return (
         <div className="ProjectWrapper">
-            <div className="Project">
-                this is my first project
+            <div className="Project" id="bachelor">
+                <div className="ProjectInfo">
+                    bachelor thesis
+                </div>
+                <div className="Bubble">
+                    <div className="ShowMore">show details</div>
+                </div>
             </div>
-            <div className="Project">
-                just a second project
+            <div className="Project" id="zenji">
+                <div className="ProjectInfo">
+                    zenji ai
+                </div>
+                <div className="Bubble">
+                    <div className="ShowMore">show details</div>
+                </div>
             </div>
-            <div className="Project">
-                just a third project
+            <div className="Project" id="microspot">
+                <div className="ProjectInfo">
+                    microspot.ch
+                </div>
+                <div className="Bubble">
+                    <div className="ShowMore">show details</div>
+                </div>
             </div>
         </div>
     )
